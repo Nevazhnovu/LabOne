d23fc95af625edfa99fcc71a99bcfe867e8221fe
Nevazhnovu <dzyua@mail.ru>
2016-04-05 12:44:23 +0300

    first commit
    added try{} catch{}

---
+++ P1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+
+
+        //[protocol]://[server]/[resource]
+        //            http://www.devbg.org/forum/index.php
+
+        static void Main(string[] args)
+        {
+            try {
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR! " + ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
